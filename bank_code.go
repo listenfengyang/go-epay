0bcddeced28f03dbd64678fd579650d1ff4a97f1
fengyang <jane.y@logtec.com>
2026-06-30 17:13:40 +0800

    add banklist

--- bank_code.go
+++ bank_code.go
@@ -1,7 +1,46 @@
 package go_epay
 
-// BankCode 马来西亚银行编码常量
-// 用于出金请求中的 bank_id 字段
+// BankCode 银行/支付渠道编码结构体
+// 支持多币种的银行列表映射
+type BankCode struct {
+	Currency       string `json:"currency"`       // 币种，如 MYR、PHP
+	Channel        string `json:"channel"`        // 支付通道
+	Code           string `json:"code"`           // 银行/渠道编码（epay 侧）
+	Name           string `json:"name"`           // 银行/渠道名称
+	ClientBankCode string `json:"clientBankCode"` // 客户侧银行编码
+}
+
+// BankCodes 全币种银行列表
+var BankCodes = []BankCode{
+	// -------------------------------------------------------
+	// MYR - Malaysia Ringgit
+	// -------------------------------------------------------
+	{Currency: "MYR", Channel: "epay", Code: "MYRTIGER", Name: "Malayan Banking Berhad", ClientBankCode: "MBB"},
+	{Currency: "MYR", Channel: "epay", Code: "CIBBMYKL", Name: "CIMB Bank", ClientBankCode: "CIMB"},
+	{Currency: "MYR", Channel: "epay", Code: "PBBEMYKL", Name: "Public Bank", ClientBankCode: "PBB"},
+	{Currency: "MYR", Channel: "epay", Code: "HLBBMYKL", Name: "Hong Leong Bank", ClientBankCode: "HLB"},
+	{Currency: "MYR", Channel: "epay", Code: "HBMBMYKL", Name: "HSBC Bank", ClientBankCode: "HSBC"},
+	{Currency: "MYR", Channel: "epay", Code: "OCBCMYKL", Name: "OCBC Bank", ClientBankCode: "OCBC"},
+	{Currency: "MYR", Channel: "epay", Code: "RHBBMYKL", Name: "RHB Bank", ClientBankCode: "RHB"},
+	{Currency: "MYR", Channel: "epay", Code: "ARBKMYKL", Name: "AmBank", ClientBankCode: "AMB"},
+	{Currency: "MYR", Channel: "epay", Code: "PHBMMYKL", Name: "Affin Bank", ClientBankCode: "AFF"},
+	{Currency: "MYR", Channel: "epay", Code: "MFBBMYKL", Name: "Alliance Bank", ClientBankCode: "ALB"},
+	{Currency: "MYR", Channel: "epay", Code: "SCBLMYKX", Name: "Standard Chartered Bank", ClientBankCode: "SCTB"},
+	{Currency: "MYR", Channel: "epay", Code: "UOVBMYKL", Name: "United Overseas Bank", ClientBankCode: "UOB"},
+	{Currency: "MYR", Channel: "epay", Code: "BIMBMYKL", Name: "Bank Islam", ClientBankCode: ""},
+	{Currency: "MYR", Channel: "epay", Code: "BMMBMYKL", Name: "Bank Muamalat", ClientBankCode: "BMMBMYKL"},
+	{Currency: "MYR", Channel: "epay", Code: "BKRMMYKL", Name: "Bank Rakyat", ClientBankCode: ""},
+	{Currency: "MYR", Channel: "epay", Code: "BSNAMYK1", Name: "Bank Simpanan Nasional", ClientBankCode: "BSN"},
+	{Currency: "MYR", Channel: "epay", Code: "GXSPMYKL", Name: "GX Bank", ClientBankCode: ""},
+	{Currency: "MYR", Channel: "epay", Code: "TNGDMYNB", Name: "Touch N Go", ClientBankCode: ""},
+	{Currency: "MYR", Channel: "epay", Code: "BOSTMYNB", Name: "Boost", ClientBankCode: ""},
+}
+
+// -------------------------------------------------------
+// 向后兼容：保留 MYR 银行编码常量，供出金请求使用
+// -------------------------------------------------------
+
+// MYR 马来西亚银行编码常量（用于出金请求 bank_id 字段）
 const (
 	BankCodeMaybank         = "MYRTIGER" // Malayan Banking Berhad (Maybank)
 	BankCodeCIMB            = "CIBBMYKL" // CIMB Bank
@@ -23,26 +62,3 @@ const (
 	BankCodeTouchNGo        = "TNGDMYNB" // Touch N Go
 	BankCodeBoost           = "BOSTMYNB" // Boost
 )
-
-// BankCodeNames 银行编码与名称映射表
-var BankCodeNames = map[string]string{
-	BankCodeMaybank:         "Malayan Banking Berhad",
-	BankCodeCIMB:            "CIMB Bank",
-	BankCodePublicBank:      "Public Bank",
-	BankCodeHongLeong:       "Hong Leong Bank",
-	BankCodeHSBC:            "HSBC Bank",
-	BankCodeOCBC:            "OCBC Bank",
-	BankCodeRHB:             "RHB Bank",
-	BankCodeAmBank:          "AmBank",
-	BankCodeAffin:           "Affin Bank",
-	BankCodeAlliance:        "Alliance Bank",
-	BankCodeStandardCharter: "Standard Chartered Bank",
-	BankCodeUOB:             "United Overseas Bank",
-	BankCodeBankIslam:       "Bank Islam",
-	BankCodeBankMuamalat:    "Bank Muamalat",
-	BankCodeBankRakyat:      "Bank Rakyat",
-	BankCodeBSN:             "Bank Simpanan Nasional",
-	BankCodeGXBank:          "GX Bank",
-	BankCodeTouchNGo:        "Touch N Go",
-	BankCodeBoost:           "Boost",
-}
--- entity.go
+++ entity.go
@@ -7,29 +7,28 @@ package go_epay
 
 // EPayDepositReq 入金请求参数
 type EPayDepositReq struct {
-	PID         string `mapstructure:"pid"`          // 商户 ID（自动填充）
-	Type        string `mapstructure:"type"`         // 支付类型，可选，留空则跳转支付页
-	OutTradeNo  string `mapstructure:"out_trade_no"` // 商户唯一订单号
-	AccountName string `mapstructure:"account_name"` // 付款方账户持有人姓名
-	BankAccount string `mapstructure:"bank_account"` // 付款方银行账户号
-	BankName    string `mapstructure:"bank_name"`    // 收款银行名称
-	NotifyURL   string `mapstructure:"notify_url"`   // 异步回调地址（自动填充）
-	ReturnURL   string `mapstructure:"return_url"`   // 支付完成跳转地址（自动填充）
-
-	Name  string `mapstructure:"name"`  // 商品名称
-	Money string `mapstructure:"money"` // 金额，单位 MYR，保留两位小数
-	Param string `mapstructure:"param"` // 透传参数，可选
+	PID         string `mapstructure:"pid"          json:"pid"          form:"pid"`          // 商户 ID（自动填充）
+	Type        string `mapstructure:"type"         json:"type"         form:"type"`         // 支付类型，可选，留空则跳转支付页
+	OutTradeNo  string `mapstructure:"out_trade_no" json:"out_trade_no" form:"out_trade_no"` // 商户唯一订单号
+	AccountName string `mapstructure:"account_name" json:"account_name" form:"account_name"` // 付款方账户持有人姓名
+	BankAccount string `mapstructure:"bank_account" json:"bank_account" form:"bank_account"` // 付款方银行账户号
+	BankName    string `mapstructure:"bank_name"    json:"bank_name"    form:"bank_name"`    // 收款银行名称
+	NotifyURL   string `mapstructure:"notify_url"   json:"notify_url"   form:"notify_url"`   // 异步回调地址（自动填充）
+	ReturnURL   string `mapstructure:"return_url"   json:"return_url"   form:"return_url"`   // 支付完成跳转地址（自动填充）
+	Name        string `mapstructure:"name"         json:"name"         form:"name"`         // 商品名称
+	Money       string `mapstructure:"money"        json:"money"        form:"money"`        // 金额，单位 MYR，保留两位小数
+	Param       string `mapstructure:"param"        json:"param"        form:"param"`        // 透传参数，可选
 }
 
 // EPayDepositRsp 入金响应
 type EPayDepositRsp struct {
-	Code      int    `json:"code"`      // 1=成功，其他失败
-	Msg       string `json:"msg"`       // 消息
-	TradeNo   string `json:"trade_no"`  // 平台订单号
-	PayURL    string `json:"payurl"`    // 支付链接（与 qrcode/urlscheme 三选一）
-	QRCode    string `json:"qrcode"`    // 二维码链接
-	URLScheme string `json:"urlscheme"` // 小程序 scheme
-	Param     string `json:"param"`     // 透传参数，如银行卡信息
+	Code      int    `json:"code"      form:"code"`      // 1=成功，其他失败
+	Msg       string `json:"msg"       form:"msg"`       // 消息
+	TradeNo   string `json:"trade_no"  form:"trade_no"`  // 平台订单号
+	PayURL    string `json:"payurl"    form:"payurl"`    // 支付链接（与 qrcode/urlscheme 三选一）
+	QRCode    string `json:"qrcode"    form:"qrcode"`    // 二维码链接
+	URLScheme string `json:"urlscheme" form:"urlscheme"` // 小程序 scheme
+	Param     string `json:"param"     form:"param"`     // 透传参数，如银行卡信息
 }
 
 // ============================================================
@@ -39,24 +38,24 @@ type EPayDepositRsp struct {
 // EPayWithdrawReq 出金请求参数
 // POST https://api.epay365.biz/payout.php
 type EPayWithdrawReq struct {
-	PID         string `mapstructure:"pid"`          // 商户 ID（自动填充）
-	Type        string `mapstructure:"type"`         // 出金类型: 1=personal, 2=corporate
-	OutTradeNo  string `mapstructure:"out_trade_no"` // 商户唯一订单号
-	NotifyURL   string `mapstructure:"notify_url"`   // 出金回调地址（自动填充）
-	Name        string `mapstructure:"name"`         // 收款人全名
-	BankID      string `mapstructure:"bank_id"`      // 银行编码（见 Bank Codes Table）
-	BankAccount string `mapstructure:"bank_account"` // 收款银行账户
-	Branch      string `mapstructure:"branch"`       // 支行名称
-	Money       string `mapstructure:"money"`        // 金额，单位 MYR，格式: 100.00
-	ClientIP    string `mapstructure:"clientip"`     // 发起方 IP
-	Param       string `mapstructure:"param"`        // 业务扩展参数（可选）
+	PID         string `mapstructure:"pid"          json:"pid"          form:"pid"`          // 商户 ID（自动填充）
+	Type        string `mapstructure:"type"         json:"type"         form:"type"`         // 出金类型: 1=personal, 2=corporate
+	OutTradeNo  string `mapstructure:"out_trade_no" json:"out_trade_no" form:"out_trade_no"` // 商户唯一订单号
+	NotifyURL   string `mapstructure:"notify_url"   json:"notify_url"   form:"notify_url"`   // 出金回调地址（自动填充）
+	Name        string `mapstructure:"name"         json:"name"         form:"name"`         // 收款人全名
+	BankID      string `mapstructure:"bank_id"      json:"bank_id"      form:"bank_id"`      // 银行编码（见 Bank Codes Table）
+	BankAccount string `mapstructure:"bank_account" json:"bank_account" form:"bank_account"` // 收款银行账户
+	Branch      string `mapstructure:"branch"       json:"branch"       form:"branch"`       // 支行名称
+	Money       string `mapstructure:"money"        json:"money"        form:"money"`        // 金额，单位 MYR，格式: 100.00
+	ClientIP    string `mapstructure:"clientip"     json:"clientip"     form:"clientip"`     // 发起方 IP
+	Param       string `mapstructure:"param"        json:"param"        form:"param"`        // 业务扩展参数（可选）
 }
 
 // EPayWithdrawRsp 出金响应
 type EPayWithdrawRsp struct {
-	Code    int    `json:"code"`
-	Msg     string `json:"msg"`
-	TradeNo string `json:"trade_no"`
+	Code    int    `json:"code"     form:"code"`     // 1=成功，其他失败
+	Msg     string `json:"msg"      form:"msg"`      // 消息
+	TradeNo string `json:"trade_no" form:"trade_no"` // 平台订单号
 }
 
 // ============================================================
@@ -65,16 +64,16 @@ type EPayWithdrawRsp struct {
 
 // EPayCallbackReq 回调通知参数
 type EPayCallbackReq struct {
-	PID         string `json:"pid"          form:"pid"`
-	TradeNo     string `json:"trade_no"     form:"trade_no"`
-	OutTradeNo  string `json:"out_trade_no" form:"out_trade_no"`
-	Type        string `json:"type"         form:"type"`
-	Name        string `json:"name"         form:"name"`
-	Money       string `json:"money"        form:"money"`
-	TradeStatus string `json:"trade_status" form:"trade_status"`
-	Param       string `json:"param"        form:"param"`
-	Sign        string `json:"sign"         form:"sign"`
-	SignType    string `json:"sign_type"    form:"sign_type"`
+	PID         string `json:"pid"          form:"pid"`          // 商户 ID
+	TradeNo     string `json:"trade_no"     form:"trade_no"`     // 平台订单号
+	OutTradeNo  string `json:"out_trade_no" form:"out_trade_no"` // 商户订单号
+	Type        string `json:"type"         form:"type"`         // 支付类型
+	Name        string `json:"name"         form:"name"`         // 商品名称
+	Money       string `json:"money"        form:"money"`        // 金额
+	TradeStatus string `json:"trade_status" form:"trade_status"` // 交易状态：success=成功
+	Param       string `json:"param"        form:"param"`        // 透传参数
+	Sign        string `json:"sign"         form:"sign"`         // 签名
+	SignType    string `json:"sign_type"    form:"sign_type"`    // 签名类型，固定 MD5
 }
 
 // ============================================================
@@ -83,39 +82,39 @@ type EPayCallbackReq struct {
 
 // EPayQueryOrderRsp 查询订单响应
 type EPayQueryOrderRsp struct {
-	Code        int     `json:"code"`
-	Msg         string  `json:"msg"`
-	PID         string  `json:"pid"`
-	TradeNo     string  `json:"trade_no"`
-	OutTradeNo  string  `json:"out_trade_no"`
-	Type        string  `json:"type"`
-	Money       float64 `json:"money"`
-	TradeStatus string  `json:"trade_status"`
+	Code        int     `json:"code"         form:"code"`         // 1=成功，其他失败
+	Msg         string  `json:"msg"          form:"msg"`          // 消息
+	PID         string  `json:"pid"          form:"pid"`          // 商户 ID
+	TradeNo     string  `json:"trade_no"     form:"trade_no"`     // 平台订单号
+	OutTradeNo  string  `json:"out_trade_no" form:"out_trade_no"` // 商户订单号
+	Type        string  `json:"type"         form:"type"`         // 支付类型
+	Money       float64 `json:"money"        form:"money"`        // 金额
+	TradeStatus string  `json:"trade_status" form:"trade_status"` // 交易状态：success=成功
 }
 
 // EPayDepositViaReq Create Payment via API 请求参数
 type EPayDepositViaReq struct {
-	PID               string `mapstructure:"pid"`
-	Type              string `mapstructure:"type"`
-	OutTradeNo        string `mapstructure:"out_trade_no"`
-	AccountName       string `mapstructure:"account_name"`
-	BankAccount       string `mapstructure:"bank_account"`
-	BankName          string `mapstructure:"bank_name"`
-	NotifyURL         string `mapstructure:"notify_url"`
-	ReturnURL         string `mapstructure:"return_url"`
-	WithdrawNotifyUrl string `mapstructure:"withdraw_notify_url"`
-	Name              string `mapstructure:"name"`
-	Money             string `mapstructure:"money"`
-	ClientIP          string `mapstructure:"clientip"`
-	Device            string `mapstructure:"device"`
-	Param             string `mapstructure:"param"`
+	PID               string `mapstructure:"pid"                 json:"pid"                 form:"pid"`                 // 商户 ID（自动填充）
+	Type              string `mapstructure:"type"                json:"type"                form:"type"`                // 支付类型
+	OutTradeNo        string `mapstructure:"out_trade_no"        json:"out_trade_no"        form:"out_trade_no"`        // 商户唯一订单号
+	AccountName       string `mapstructure:"account_name"        json:"account_name"        form:"account_name"`        // 付款方账户持有人姓名
+	BankAccount       string `mapstructure:"bank_account"        json:"bank_account"        form:"bank_account"`        // 付款方银行账户号
+	BankName          string `mapstructure:"bank_name"           json:"bank_name"           form:"bank_name"`           // 收款银行名称
+	NotifyURL         string `mapstructure:"notify_url"          json:"notify_url"          form:"notify_url"`          // 异步回调地址（自动填充）
+	ReturnURL         string `mapstructure:"return_url"          json:"return_url"          form:"return_url"`          // 支付完成跳转地址（自动填充）
+	WithdrawNotifyUrl string `mapstructure:"withdraw_notify_url" json:"withdraw_notify_url" form:"withdraw_notify_url"` // 出金回调地址
+	Name              string `mapstructure:"name"                json:"name"                form:"name"`                // 商品名称
+	Money             string `mapstructure:"money"               json:"money"               form:"money"`               // 金额，单位 MYR，保留两位小数
+	ClientIP          string `mapstructure:"clientip"            json:"clientip"            form:"clientip"`            // 发起方 IP
+	Device            string `mapstructure:"device"              json:"device"              form:"device"`              // 设备类型，如 pc / mobile
+	Param             string `mapstructure:"param"               json:"param"               form:"param"`               // 透传参数，可选
 }
 
 // EPayDepositViaRsp Create Payment via API 响应
 type EPayDepositViaRsp struct {
-	Code    int    `json:"code"`
-	Msg     string `json:"msg"`
-	TradeNo string `json:"trade_no"`
-	PayURL  string `json:"payurl"`
-	Param   string `json:"param"`
+	Code    int    `json:"code"     form:"code"`     // 1=成功，其他失败
+	Msg     string `json:"msg"      form:"msg"`      // 消息
+	TradeNo string `json:"trade_no" form:"trade_no"` // 平台订单号
+	PayURL  string `json:"payurl"   form:"payurl"`   // 支付链接
+	Param   string `json:"param"    form:"param"`    // 透传参数
 }
