ab0ec41e5282bfb398a27e1948a53cd3f5e125f0
fengyang <jane.y@logtec.com>
2026-07-01 11:10:12 +0800

    fix: move path constants to default.go so SDK works without constant.go

---
+++ default.go
@@ -0,0 +1,8 @@
+package go_epay
+
+const (
+	DefaultBaseURL = "https://api.epay365.biz"
+	DepositPath    = "/submit.php" // Hosted Page 入金
+	DepositViaPath = "/mapi.php"   // Server-to-Server API 入金
+	WithdrawPath   = "/payout.php" // 出金
+)
